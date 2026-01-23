4dcce0dfc95e9af0bb95190a85b9632b2513cdb5
Kaszás Szófia <szofia.kaszas@gmail.com>
2026-01-23 22:15:26 +0100

    login done?

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,34 +4,42 @@ import { AuthContext } from "../context/authContext";
 export function Login() {
   const context = useContext(AuthContext);
 
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-  return (
-    <>
-    <form onSubmit={(e) => {
-      e.preventDefault();
-      context.login(email, password);
-    }}>
-      <h1>Login Page</h1>
-      <div>
-        <label htmlFor="email">Email:</label>
-        <input onChange={
-          (e) => setEmail(e.target.value === "" ? "" : e.target.value)
-        } type="email" id="email" name="email" required />
-      </div>
-      <div>
-        <label htmlFor="password">Password:</label>
-        <input onChange={
-          (e) => setPassword(e.target.value === "" ? "" : e.target.value)
-        } type="password" id="password" name="password" required />
-      </div>
-      <button type="submit">Login</button>
-    </form>
+  if (!context.currentUser) {
+    return (
+      <>
+        <form
+          onSubmit={async (e) => {
+            e.preventDefault();
+            const form = new FormData(e.currentTarget);
+            try {
+              await context.login(
+                form.get("email") as string,
+                form.get("password") as string,
+              );
+              setError("");
+            } catch (err) {
+              setError((err as Error).message);
+            }
+          }}
+        >
+          <h1>Login Page</h1>
+          <div>
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" name="email" required />
+          </div>
+          <div>
+            <label htmlFor="password">Password:</label>
+            <input type="password" id="password" name="password" required />
+          </div>
+          <button type="submit">Login</button>
+        </form>
 
-    <div>
-      <a href="/register">Don't have an account? Register here.</a>
-    </div>
-    </>
-  );
+        <div>
+          <a href="/register">Don't have an account? Register here.</a>
+        </div>
+      </>
+    );
+  }
 }
