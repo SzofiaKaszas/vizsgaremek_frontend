9ec3803b523b98a7eaa9765aa285fe37ecd266aa
SzofiaKaszas <szofia.kaszas@gmail.com>
2026-02-10 09:04:59 +0100

    basic look for navBar and Register done

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Outlet } from 'react-router'
 import { NavBar } from './pages/Nav'
 import { useContext } from 'react'
 import { AuthContext } from './context/authContext'
+import './App.css'
 
 function App() {
   const context = useContext(AuthContext)
